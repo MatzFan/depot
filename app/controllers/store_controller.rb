c3d2eebef1a1b065bca583ab05c8a1b60621f705
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-10 18:14:03 +0000

    Added PG gem to production group for Heroku

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.order(:title) # order by product title
+    @products = Product.order(:title) # order all by product title
+    puts @products.inspect
   end
 end
