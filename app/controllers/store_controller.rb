b96b2c5f76458ee721e7aaf0c963601793dc9b1e
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-12 20:01:01 +0000

    Moved cart display to sidebar - no longer on separate page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart # provides access to @cart
+  before_action :set_cart
   def index
     @products = Product.order(:title) # order all by product title
   end
