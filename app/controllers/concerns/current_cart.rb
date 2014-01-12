1f211e1150ae2bc219ac2903126748985b58d645
Bruce Steedman <bruce.steedman@gmail.com>
2014-01-12 18:54:21 +0000

    Played with application_controller protect_from_forgery settings - left as :exception strategy

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -7,7 +7,7 @@ module CurrentCart
       @cart = Cart.find(session[:cart_id]) # this is how Rails stores a seesion - as a Hash
     rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
-      session[:cart_id] = @cart.id
+      session[:cart_id] = @cart.id # sets param for this cart in a cookie
     end
 
 end # of module
